c85a80725045c708f21f1c5d959bd571ed2bec36
Henrique Ferreiro <hferreiro@udc.es>
2014-01-24 14:08:52 +0100

    Update BinaryTrees to latest version

--- Shootout/BinaryTrees/binarytrees.hs
+++ Shootout/BinaryTrees/binarytrees.hs
@@ -1,61 +1,57 @@
+{-# LANGUAGE BangPatterns #-}
 --
--- The Great Computer Language Shootout
--- http://shootout.alioth.debian.org/
+-- The Computer Language Benchmarks Game
+-- http://benchmarksgame.alioth.debian.org/
 --
 -- Contributed by Don Stewart
 --
 
 import System.Environment
+import Data.Bits
+import Text.Printf
 
-import Fibon.Run.BenchmarkHelper
+--
+-- an artificially strict tree.
+--
+-- normally you would ensure the branches are lazy, but this benchmark
+-- requires strict allocation.
+--
+data Tree = Nil | Node !Int !Tree !Tree
 
-data Tree = Nil | Node !Int Tree Tree
-
-minN :: Int
 minN = 4
 
-io i s n t = 
-  if i == 1 then
-    putStrLn $ s ++ " of depth "++show n++ "\t check: " ++ show t
-  else
-    s `deepseq` n `deepseq` t `deepseq` return ()
+io s n t = printf "%s of depth %d\t check: %d\n" s n t
 
-main = fibonMain oldmain
-
-oldmain 0 = return ()
-oldmain cnt = do
-    n <- fmap (read . head) getArgs
+main = do
+    n <- getArgs >>= readIO . head
     let maxN     = max (minN + 2) n
         stretchN = maxN + 1
 
     -- stretch memory tree
     let c = check (make 0 stretchN)
-    io cnt "stretch tree" stretchN c
+    io "stretch tree" stretchN c
 
     -- allocate a long lived tree
-    let long    = make 0 maxN
+    let !long    = make 0 maxN
 
     -- allocate, walk, and deallocate many bottom-up binary trees
     let vs = depth minN maxN
-    mapM_ (\((m,d,i)) -> io cnt (show m ++ "\t trees") d i) vs
+    mapM_ (\((m,d,i)) -> io (show m ++ "\t trees") d i) vs
 
     -- confirm the the long-lived binary tree still exists
-    io cnt "long lived tree" maxN (check long)
-
-    oldmain (cnt-1)
+    io "long lived tree" maxN (check long)
 
 -- generate many trees
 depth :: Int -> Int -> [(Int,Int,Int)]
 depth d m
     | d <= m    = (2*n,d,sumT d n 0) : depth (d+2) m
     | otherwise = []
-  where n = 2 ^ (m - d + minN) :: Int
+  where n = 1 `shiftL` (m - d + minN)
 
 -- allocate and check lots of trees
 sumT :: Int -> Int -> Int -> Int
-sumT  d i t | d `seq` False = undefined -- strictness hint
-sumT  d 0 t = t
-sumT  d i t = sumT d (i-1) (t + a + b)
+sumT d 0 t = t
+sumT d i t = sumT d (i-1) (t + a + b)
   where a = check (make i    d)
         b = check (make (-i) d)
 
@@ -69,4 +65,3 @@ make :: Int -> Int -> Tree
 make i 0 = Node i Nil Nil
 make i d = Node i (make (i2-1) d2) (make i2 d2)
   where i2 = 2*i; d2 = d-1
-
